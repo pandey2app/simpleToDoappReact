8007c3449b1af8cf4d792c7cfb6257482bff2323
aditya prakash pandey <pandey2app@gmail.com>
2024-06-01 10:03:41 +0000

    added svg image for icon

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,9 +1,10 @@
 import React from 'react'
+import checkIcon from "../images/check.svg"
 
 export default function Header() {
   return (
     <div className='w-full border-b-4 border-red-600 text-center mb-4 p-3 bg-yellow-300'>
-        <h1 className='text-4xl font-extrabold font-sherif text-red-600'>Check It <span className='text-green-700'>&#x1F5F9;</span></h1>
+        <h1 className='text-4xl font-extrabold font-sherif flex justify-center text-red-600'>Check It <img className='h-10 w-10 ms-4 ' src={checkIcon}/></h1>
         <p className='mt-2 text-xl min-md:text-2xl font-mono font-bold text-green-700'>Daily Tasks Organised...!</p>
       
     </div>
