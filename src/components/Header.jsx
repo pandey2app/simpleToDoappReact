86330e8c1fc00d2bc6d4d06d89db4ce6d0f8fba0
aditya prakash pandey <pandey2app@gmail.com>
2024-06-01 09:23:27 +0000

    creted heding component

---
+++ src/components/Header.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+export default function Header() {
+  return (
+    <div className='w-full border-b-4 border-red-600 text-center mb-4 p-3 bg-yellow-300'>
+        <h1 className='text-4xl font-extrabold font-sherif text-red-600'>Check It <span className='text-green-700'>&#x1F5F9;</span></h1>
+        <p className='mt-2 text-2xl font-mono font-bold text-green-700'>Daily Tasks Organised...!</p>
+      
+    </div>
+  )
+}
