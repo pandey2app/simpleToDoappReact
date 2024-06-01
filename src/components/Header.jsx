81a40adf0fd72ea7ac350a819e4a372a2d5ee20f
aditya prakash pandey <pandey2app@gmail.com>
2024-06-01 09:35:53 +0000

    fixed slogan font

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -4,7 +4,7 @@ export default function Header() {
   return (
     <div className='w-full border-b-4 border-red-600 text-center mb-4 p-3 bg-yellow-300'>
         <h1 className='text-4xl font-extrabold font-sherif text-red-600'>Check It <span className='text-green-700'>&#x1F5F9;</span></h1>
-        <p className='mt-2 text-2xl font-mono font-bold text-green-700'>Daily Tasks Organised...!</p>
+        <p className='mt-2 text-xl min-md:text-2xl font-mono font-bold text-green-700'>Daily Tasks Organised...!</p>
       
     </div>
   )
